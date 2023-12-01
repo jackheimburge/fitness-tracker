d6c4a1ea8ce797133495cd649dc858aca51c6af8
centipedejam <jheimburge@gmail.com>
2023-12-01 15:16:41 -0800

    remove order routes from App.jsx

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -4,8 +4,6 @@ import { Routes, Route } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
 import './App.css';
 import AuthPage from '../AuthPage/AuthPage';
-import NewOrderPage from '../NewOrderPage/NewOrderPage';
-import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
 import NavBar from '../../components/NavBar/NavBar';
 
 export default function App() {
@@ -18,8 +16,6 @@ export default function App() {
           <NavBar user={user} setUser={setUser} />
           <Routes>
             {/* Route components in here */}
-            <Route path="/orders/new" element={<NewOrderPage />} />
-            <Route path="/orders" element={<OrderHistoryPage />} />
           </Routes>
         </>
         :
